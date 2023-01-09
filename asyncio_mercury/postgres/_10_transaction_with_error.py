49ee24741759089e41614a4946f7b5838ea01fa8
iteegi <61802277+iteegi@users.noreply.github.com>
2023-01-09 22:57:00 +0300

    create _10_transaction_with_error.py

---
+++ asyncio_mercury/postgres/_10_transaction_with_error.py
@@ -0,0 +1,28 @@
+import asyncio
+import logging
+import asyncpg
+
+
+async def main():
+    connection = await asyncpg.connect(host='192.168.180.128',
+                                       port=5432,
+                                       user='postgres',
+                                       database='products',
+                                       password='postgres')
+    try:
+        async with connection.transaction():
+            insert_brand = "INSERT INTO brand VALUES(9999, 'big_brand')"
+            await connection.execute(insert_brand)
+            await connection.execute(insert_brand)
+    except Exception:
+        logging.exception('Error while running transaction')
+    finally:
+        query = """SELECT brand_name FROM brand 
+                    WHERE brand_name LIKE 'big_%'"""
+        brands = await connection.fetch(query)
+        print(f'Query result was: {brands}')
+
+        await connection.close()
+
+
+asyncio.run(main())
