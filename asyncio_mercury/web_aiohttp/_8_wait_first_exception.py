6df06675c6933192b13e2fd6494ae47e018a6418
iteegi <61802277+iteegi@users.noreply.github.com>
2023-01-09 15:42:47 +0300

    create _8_wait_first_exception.py

---
+++ asyncio_mercury/web_aiohttp/_8_wait_first_exception.py
@@ -0,0 +1,36 @@
+import asyncio
+import aiohttp
+import sys
+import logging
+
+from aiohttp import ClientSession
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+from _3_time_out import fetch_status
+from utilq.async_timer import async_timed
+
+
+
+@async_timed()
+async def main():
+    async with aiohttp.ClientSession() as session:
+
+        fetchers = [asyncio.create_task(fetch_status(session, 'https://bad')),
+                    asyncio.create_task(fetch_status(session, 'https://www.vkusnyblog.ru/', delay=3)),
+                    asyncio.create_task(fetch_status(session, 'https://www.vkusnyblog.ru/', delay=3))]
+        
+        done, pending = await asyncio.wait(fetchers, return_when=asyncio.FIRST_EXCEPTION)
+                
+        print(f'Done task count: {len(done)}')
+        print(f'Pending task count: {len(pending)}')
+
+        for done_task in done:
+            if done_task.exception() is None:
+                print(done_task.result())
+            else:
+                logging.error('Exception', exc_info=done_task.exception())
+        for pending_task in pending:
+            pending_task.cancel()
+        
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+asyncio.run(main())
